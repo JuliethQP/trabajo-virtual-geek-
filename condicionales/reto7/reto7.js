07be5c62dc92e338682323af6e3c51ad68c86e4e
Julieth <juliethquinchia@gmail.com>
2020-03-17 09:53:03 -0500

    Sprint [1] trabajo virtual

--- condicionales/reto7/reto7.js
+++ condicionales/reto7/reto7.js
@@ -1,8 +1,14 @@
-const año;
-año=parseInt(prompt("Cual año quieres saber"));
-if(año==0 || año==4 || año==8 || año==12){
-    console.log("es el año bisiesto")
+var año=1901;
+
+if(año%4==0 && año%400==0 && año%100==0){
+    console.log("el año es bisiesto");
+
+    if(año%4==0){
+        console.log("año bisiesto");
+    }
 }
-else if (año%2!=0){
-    console.log("el siguiente es el año bisiesto" + año+1);
+
+else if (año%4!=0 || año%400!=0 || año%100!=0){
+    año=año+1;
+    console.log("el siguiente es el año bisiesto " + año);
 }
